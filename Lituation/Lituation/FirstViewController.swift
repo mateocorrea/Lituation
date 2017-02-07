8817a7517217670d9a65fc24d3408a2522bdf1bd
mateocorrea <mateo8correa@gmail.com>
2017-02-07 14:08:12 -0500

    Initial Xcode Project Files

---
+++ Lituation/Lituation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lituation
+//
+//  Created by Mateo Correa on 2/7/17.
+//  Copyright © 2017 mixxrio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
