0bb66fd0e3206366810dbe9caba4f2f1503a81b4
chriswill21 <christien.williams@gmail.com>
2017-05-19 19:55:04 -0400

    print a
    print a

--- Lituation/Lituation/FirstViewController.swift
+++ Lituation/Lituation/FirstViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
     let first = 2
+    print('a')
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
